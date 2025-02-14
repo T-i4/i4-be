67069efb2d1cf241e2f7dddbfda67d59d6ebeada
sosa7753 <sosa7753@naver.com>
2025-02-14 12:31:56 +0900

    initial commit

---
+++ src/main/java/com/business/i4_be/I4BeApplication.java
@@ -0,0 +1,13 @@
+package com.business.i4_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I4BeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(I4BeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/business/i4_be/I4BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business.i4_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I4BeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
