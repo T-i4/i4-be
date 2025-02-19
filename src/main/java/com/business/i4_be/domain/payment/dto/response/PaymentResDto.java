f5a08f880c89f49a3c0e8ee10bb99e0c9eeed912
sall6550 <keinpark1@naver.com>
2025-02-19 15:45:48 +0900

    fix: @Data 사용 지양

--- src/main/java/com/business/i4_be/domain/payment/dto/response/PaymentResDto.java
+++ src/main/java/com/business/i4_be/domain/payment/dto/response/PaymentResDto.java
@@ -1,17 +1,17 @@
 package com.business.i4_be.domain.payment.dto.response;
 
 import com.business.i4_be.domain.payment.constant.PaymentStatus;
-import lombok.Data;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
-@Data
+@Getter
 public class PaymentResDto {
     private List<PaymentDetail> payments;
 
-    @Data
+    @Getter
     public static class PaymentDetail {
         private UUID paymentId;
         private Long userId;
