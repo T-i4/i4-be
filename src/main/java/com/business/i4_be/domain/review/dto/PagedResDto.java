e775a78f3cf4a101868ec8f1798d18fc53b6ca4f
chaewKim <anjh7127@gmail.com>
2025-02-24 12:18:22 +0900

    PagedResDto 추가

---
+++ src/main/java/com/business/i4_be/domain/review/dto/PagedResDto.java
@@ -0,0 +1,29 @@
+package com.business.i4_be.domain.review.dto;
+
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+@Getter
+public class PagedResDto<T> {
+    private final List<T> content;
+    private final int totalPages;
+    private final long totalElements;
+    private final int size;
+    private final int pageNumber;
+    private final int numberOfElements;
+    private final boolean first;
+    private final boolean last;
+
+    public PagedResDto(Page<T> page) {
+        this.content = page.getContent();
+        this.totalPages = page.getTotalPages();
+        this.totalElements = page.getTotalElements();
+        this.size = page.getSize();
+        this.pageNumber = page.getNumber();
+        this.numberOfElements = page.getNumberOfElements();
+        this.first = page.isFirst();
+        this.last = page.isLast();
+    }
+}
